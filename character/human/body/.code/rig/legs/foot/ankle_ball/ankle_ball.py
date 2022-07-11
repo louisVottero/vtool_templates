16ca505260060ed248d479be48a4e58c0c3a4302
pumacamargo <puma.camargo@gmail.com>
2022-07-11 08:17:29 -0700

    Improvement: mapped all lines in ankle_ball.py
    I missed a couple of more lines that needed to be mapped aswell.

--- character/human/body/.code/rig/legs/foot/ankle_ball/ankle_ball.py
+++ character/human/body/.code/rig/legs/foot/ankle_ball/ankle_ball.py
@@ -5,18 +5,20 @@ def main():
     for side in 'LR':
         loc_ankle_ball = cmds.spaceLocator(n = 'aim_ankleBall_%s' % side)[0]
         jnt_ankleball = put.joint_foot[side]['ankleball']
+        jnt_ball = put.joint_foot[side]['foot'][1]
+
         space.MatchSpace(jnt_ankleball, loc_ankle_ball).translation_rotation()
         cmds.parent(loc_ankle_ball, 'follow_controls_foot_1_%s' % side)
         cmds.hide(loc_ankle_ball)
         
-        cmds.aimConstraint('JNT_ball_%s' % side,
+        cmds.aimConstraint(jnt_ball,
                             loc_ankle_ball,
                             aimVector = [0,0,1],
-                            wuo = 'JNT_ball_%s' % side, 
+                            wuo = jnt_ball, 
                             wut = 'objectrotation')
                             
-        cmds.orientConstraint(loc_ankle_ball, 'JNT_ankleBall_%s' % side, mo = True)
+        cmds.orientConstraint(loc_ankle_ball, jnt_ankleball, mo = True)
     
         #cmds.addAttr('CNT_IK_LEG_BTM_1_%s' % side, ln = 'ankleRoll', k = True)
         #cmds.connectAttr('CNT_IK_LEG_BTM_1_%s.ankleRoll' % side, 
-        #                    'driver_CNT_IK_FOOT_BALL_1_%s.rotateZ' % side)
+        #     
