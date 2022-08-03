bd3268378173d34d89edda746aaf0eb0d9f3abfc
Louis Vottero <louis.vot@gmail.com>
2022-08-03 00:27:41 -0400

    meta human fixes and twist setup in non meta human setup

--- character/human/body/.code/init/init.py
+++ character/human/body/.code/init/init.py
@@ -1,5 +1,3 @@
 
 def main():
-    
-    put.size = process.get_option('Size')
-    put.scale_uniform = process.get_option('Scale Uniform')
+    return
---
+++ character/human/body/.code/rig/twist/twist_elbow/twist_elbow.py
@@ -0,0 +1,37 @@
+from vtool.maya_lib import rigs
+from vtool.maya_lib import space
+
+def main():
+
+    put.joint_twist_elbow = {}
+
+    for side in 'LR':    
+        
+        size = put.size
+                        
+        arm_joints = put.joint_arm[side]
+        elbow_joint = arm_joints[1]
+        
+        rig = rigs.TwistRig('elbow_twist', side)
+        rig.set_joints(elbow_joint)
+        rig.set_control_size(size*15)
+        rig.set_control_offset_axis('Z')
+        rig.set_control_shape('square')
+        rig.set_parent_sub_joints(False)
+        rig.set_twist_joint_name('JNT_twistElbow_1_%s' % side)
+
+        rig.set_control_set([side,'arm_%s' % side])                
+        rig.set_control_parent(put.control_clavicle[side])
+        rig.set_setup_parent(put.group_setup)
+        rig.set_control_count(put.twist_elbow_count)
+        rig.connect_sub_visibility('%s.subVisibility' % put.control_settings)
+        rig.create()
+        
+        arm_twist_btm_locator = put.attach_bottom_twist_arm[side]
+        
+        cmds.parentConstraint(rig.controls[0], arm_twist_btm_locator, mo = True)
+
+        space.create_follow_group(elbow_joint, 'xform_CNT_ELBOW_TWIST_TOP_1_%s' % side)        
+        space.create_follow_group(arm_joints[2], 'xform_CNT_ELBOW_TWIST_BTM_1_%s' % side)              
+        
+        put.joint_twist_elbow[side] = rig.sub_joints                
