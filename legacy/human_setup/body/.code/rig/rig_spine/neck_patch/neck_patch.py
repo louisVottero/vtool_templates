1e6e33b1a8de92b5e270201ab2c065a9ea201173
Louis Votter <louis.vot@gmail.com>
2025-03-31 13:59:20 -0400

    removed pymel from legacy / body

--- legacy/human_setup/body/.code/rig/rig_spine/neck_patch/neck_patch.py
+++ legacy/human_setup/body/.code/rig/rig_spine/neck_patch/neck_patch.py
@@ -1,11 +1,11 @@
-import pymel.core as pmc
+import maya.cmds as cmds
 
 def main():
     # unhide neck control
-    pmc.setAttr('xform_CNT_NECK_3_C.v', 1)
+    cmds.setAttr('xform_CNT_NECK_3_C.v', 1)
     # move neck twist to include head control
-    pmc.parent('locatorTwistEnd_neck_1_C', 'CNT_SUB_HEAD_1_C')
-    point_const = pmc.pointConstraint('joint_head', 'locatorTwistEnd_neck_1_C')
-    pmc.delete(point_const)
+    cmds.parent('locatorTwistEnd_neck_1_C', 'CNT_SUB_HEAD_1_C')
+    point_const = cmds.pointConstraint('joint_head', 'locatorTwistEnd_neck_1_C')
+    cmds.delete(point_const)
     
     
