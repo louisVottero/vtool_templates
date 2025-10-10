951c58c6fa5dc5374c46f57a0b31603204c1ba9c
Louis Votter <louis.vot@gmail.com>
2025-10-10 09:48:06 -0400

    minor updates

--- legacy/human_setup/face/blends/.code/split_shapes/split_brow/split_brow.py
+++ legacy/human_setup/face/blends/.code/split_shapes/split_brow/split_brow.py
@@ -2,7 +2,7 @@
 def main():
     
     split_shape = process.get_runtime_value('split_shape')
-
+    print('split shape', split_shape)
     split_shape("browUp", width=2)
     split_shape("browDn", width=2)
     split_shape("corrugator", width=0.5)
